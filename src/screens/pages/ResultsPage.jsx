a9e3251fd3086aaf244881ef82e578594847d191
Jabulani <jabuk21@gmail.com>
2020-10-30 09:54:06 +0200

    test complete

--- src/screens/pages/ResultsPage.jsx
+++ src/screens/pages/ResultsPage.jsx
@@ -2,17 +2,24 @@ import React from 'react'
 import {connect} from 'react-redux';
 import {firestoreConnect} from 'react-redux-firebase'
 import {compose} from "redux";
+import {NavLink} from 'react-router-dom';
 
 function ResultsPage(props) {
     const surveys= props.surveys
+    var countSurveys=0
     var youngest=120
     var oldest= 0
-  
-    const nummberOfSurveys=()=>{
-       
-        const count=surveys.length
-        console.log(count)
-        return count
+    var pizzaPercentage=0
+    var pastaPercentage=0
+    var papAndWorsPercentage=0
+    var eatOutAvg=0
+    var watchMoviesAvg=0
+    var watchTvAvg=0
+    var radioListenersAvg=0
+
+    if (surveys){
+    countSurveys=surveys.length
+
     }
     const countAge=()=>{
         var age=0
@@ -33,17 +40,79 @@ function ResultsPage(props) {
             ageAverage=sum/numberOfParticipants
             return ageAverage
     }
+    const preferenceCount=()=>{
+        var pizzaNumber=0
+        var pastaNumber=0
+        var papAndWorsNumber=0
+       surveys.forEach(survey=>{
+           const food= survey.favourite_food
+           console.log(food)
+            food.forEach(foodtype=>{
+                if(foodtype==='pizza'){
+                pizzaNumber++
+            }else if(foodtype==='pasta'){
+                pastaNumber++
+            }else if(foodtype==='Pap and Wors'){
+                papAndWorsNumber++
+            }
+            })
+            
+        })
+
+        pizzaPercentage=pizzaNumber/countSurveys
+        pastaPercentage=pastaNumber/countSurveys
+        papAndWorsPercentage= papAndWorsNumber/countSurveys
+    }
+
+    const activitiesAvg=()=>{
+        var eatOutSum=0
+        var watchMoviesSum=0
+        var watchTvSum=0
+        var radioListenersSum=0
+        surveys.forEach(survey=>{
+            eatOutSum=eatOutSum+Number(survey.eat_out)
+            watchMoviesSum=watchMoviesSum+Number(survey.watch_movies)
+            watchTvSum=watchTvSum+Number(survey.watch_tv)
+            radioListenersSum=radioListenersSum+Number(survey.radio)
+        })
+
+        eatOutAvg= eatOutSum/countSurveys
+        watchMoviesAvg= watchMoviesSum/countSurveys
+        watchTvAvg= watchTvSum/countSurveys
+        radioListenersAvg= radioListenersSum/countSurveys
+    }
     return (
-        <div className="results-div">
+        
+        <div className="results-div mb-4">
+            {surveys?preferenceCount():null}
+            {surveys?activitiesAvg():null}
+
             <div className="row ">
                 <div className="col-8 m-auto">
-                    <ul class="list-group">
-                        <li class="list-group-item">Total number of surveys: <b className="ml-4">{surveys?nummberOfSurveys():null}  </b></li>
+                    <ul class="list-group mb-4">
+                        <li class="list-group-item">Total number of surveys: <b className="ml-4">{surveys?countSurveys:null}  </b></li>
                         <li class="list-group-item">Average age: <b className="ml-4">{surveys?countAge():null}</b> </li>
                         <li class="list-group-item">Oldest person who participated in survey <b className="ml-4">{oldest}</b></li>
                         <li class="list-group-item">Youngest person who participated in survey <b className="ml-4">{youngest}</b></li>
                     </ul>
+
+                    <ul class="list-group mb-4">
+                        <li class="list-group-item">Percentage of people who like Pizza <b className="ml-4">{surveys?pizzaPercentage.toFixed(1):null}  </b></li>
+                        <li class="list-group-item">Percentage of people who like Pasta: <b className="ml-4">{surveys?pastaPercentage.toFixed(1):null}</b> </li>
+                        <li class="list-group-item">Percentage of people who like Pap and Wors:<b className="ml-4">{surveys?papAndWorsPercentage.toFixed(1):null}</b></li>
+                    </ul>
+                    <ul class="list-group mb-4">
+                        <li class="list-group-item">People like to eat out: <b className="ml-4">{surveys?eatOutAvg.toFixed(1):null}  </b></li>
+                        <li class="list-group-item">People like to watch movies: <b className="ml-4">{surveys?watchMoviesAvg.toFixed(1):null}</b> </li>
+                        <li class="list-group-item">People like to watch TV: <b className="ml-4">{surveys?watchTvAvg.toFixed(1):null}</b></li>
+                        <li class="list-group-item">People like to listen to the radio: <b className="ml-4">{surveys?radioListenersAvg.toFixed(1):null}</b></li>
+                    </ul>
                 </div>
+            </div>
+            <div className="row">
+            <NavLink to='/' className="m-auto">
+                <button  class="btn btn-ok btn-primary ">OK</button>
+            </NavLink>
             </div>
         </div>
     )
